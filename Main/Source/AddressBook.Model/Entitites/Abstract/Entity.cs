1b10477f9b71c20fb88497e9390f3ab65bcbbb67
jeffflater <jeff.flater@semaphoredoes.com>
2013-09-06 18:26:42 -0500

    updated model

---
+++ Main/Source/AddressBook.Model/Entitites/Abstract/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace AddressBook.Model.Entitites.Abstract
+{
+    public abstract class Entity
+    {
+        [Key]
+        public long Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+        public DateTime LastModifiedOn { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ Main/Source/AddressBook.Model/Entitites/Abstract/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook.Model.Entitites.Abstract
+{
+    public abstract class Person : Entity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName {
+            get {
+                return string.Format("{0}, {1}", LastName, FirstName);
+            }
+        }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
