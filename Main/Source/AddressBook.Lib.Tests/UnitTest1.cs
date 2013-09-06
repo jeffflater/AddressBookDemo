fc3c461354eff1168b5c306ccd6207c474c11ccd
jeffflater <jeff.flater@semaphoredoes.com>
2013-09-06 02:09:28 -0500

    added nunit test project

---
+++ Main/Source/AddressBook.Lib.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AddressBook.Lib.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.IsFalse(false);
+        }
+    }
+}
