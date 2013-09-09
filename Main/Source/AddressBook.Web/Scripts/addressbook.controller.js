9a1b8dad59535a4afbf61daf30af80ca79050de3
jeffflater <jeff.flater@semaphoredoes.com>
2013-09-08 22:46:09 -0500

    js controller

---
+++ Main/Source/AddressBook.Web/Scripts/addressbook.controller.js
@@ -0,0 +1,90 @@
+﻿function CustomerController() {
+    this.Get = function (id, callback) {
+        $.ajax({
+            type: 'GET',
+            contentType: 'application/json',
+            dataType: 'json',
+            url: "/api/customer/" + id,
+            "sAjaxDataProp": "data.inner",
+            success: function (data) {
+                callback.apply(this, [data]);
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                debugger;
+            }
+        });
+    }
+}
+
+function EmployeeController() {
+    this.Get = function (id, callback) {
+        $.ajax({
+            type: 'GET',
+            contentType: 'application/json',
+            dataType: 'json',
+            url: "/api/employee/" + id,
+            "sAjaxDataProp": "data.inner",
+            success: function (data) {
+                callback.apply(this, [data]);
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                debugger;
+            }
+        });
+    }
+}
+
+function ManagerController() {
+    this.Get = function (id, callback) {
+        $.ajax({
+            type: 'GET',
+            contentType: 'application/json',
+            dataType: 'json',
+            url: "/api/manager/" + id,
+            "sAjaxDataProp": "data.inner",
+            success: function (data) {
+                callback.apply(this, [data]);
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                debugger;
+            }
+        });
+    }
+}
+
+function SalesPersonController() {
+    this.Get = function (id, callback) {
+        $.ajax({
+            type: 'GET',
+            contentType: 'application/json',
+            dataType: 'json',
+            url: "/api/salesperson/" + id,
+            "sAjaxDataProp": "data.inner",
+            success: function (data) {
+                callback.apply(this, [data]);
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                debugger;
+            }
+        });
+    }
+}
+
+function PeopleController() {
+    this.GetAll = function (callback) {
+        this.response = null;
+        $.ajax({
+            type: 'GET',
+            contentType: 'application/json',
+            dataType: 'json',
+            url: "/api/people",
+            "sAjaxDataProp": "data.inner",
+            success: function (data) {
+                callback.apply(this, [data]);
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                debugger;
+            }
+        });
+    }
+}
